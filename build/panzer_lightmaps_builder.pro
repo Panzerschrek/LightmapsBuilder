443af57cd57fefb2b5a0bda8b5e59b5b9908d6ed
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-08-14 15:16:40 +0700

    Sdelano sozdanije okna na osnove SDL.

--- build/panzer_lightmaps_builder.pro
+++ build/panzer_lightmaps_builder.pro
@@ -1,13 +1,20 @@
 TEMPLATE = app
-CONFIG += console
 CONFIG -= app_bundle
+CONFIG += windows
 CONFIG -= qt
 CONFIG += c++11
-
 
 CONFIG( debug, debug|release ) {
 	CONFIG+= PLB_DUBUG
 }
+
+SDL_INCLUDES_DIR = ../../SDL2-2.0.3/include
+SDL_LIBS_DIR = ../../SDL2-2.0.3/lib/x86
+
+INCLUDEPATH += $$SDL_INCLUDES_DIR
+
+LIBS += $$SDL_LIBS_DIR/SDL2main.lib
+LIBS += $$SDL_LIBS_DIR/SDL2.lib
 
 SOURCES += \
 	../src/main.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,68 @@
-int main()
+#include <iostream>
+
+#include <SDL.h>
+
+static void FatalError(const char* message)
 {
+	std::cout << message << std::endl;
+	exit(-1);
+}
+
+extern "C" int main(int argc, char *argv[])
+{
+	// TODO - work with parameters
+	(void)argc;
+	(void)argv;
+
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+		FatalError("Can not initialize sdl video");
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+	const int screen_width= 1024, screen_height= 768;
+
+	SDL_Window* const window=
+		SDL_CreateWindow(
+			"Panzerschrek Lightmaps Builder",
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			screen_width, screen_height,
+			SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+
+	if( window == nullptr )
+		FatalError( "Can not create window" );
+
+	const SDL_GLContext gl_context= SDL_GL_CreateContext(window);
+	if( gl_context == nullptr )
+		FatalError( "Can not create OpenGL context" );
+
+	SDL_GL_SetSwapInterval(1);
+
+	bool quited= false;
+	do
+	{
+		SDL_Event event;
+		while( SDL_PollEvent(&event) )
+		{
+			switch(event.type)
+			{
+			case SDL_QUIT:
+				quited= true;
+				break;
+
+			default:
+				break;
+			};
+		}
+
+		SDL_GL_SwapWindow(window);
+
+	}while(!quited);
+
+	SDL_Quit();
 	return 0;
 }
