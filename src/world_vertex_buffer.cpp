ffecf8c08090954c7f40c4626bf0c8d287e25ee6
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-09-02 21:11:42 +0700

    Krivyje poverhnosti teperj tože mogut izlucatj svet.

--- src/world_vertex_buffer.cpp
+++ src/world_vertex_buffer.cpp
@@ -212,5 +212,28 @@ void plb_WorldVertexBuffer::PrepareLuminousPolygons(
 		}
 	} // for polygons
 
+	for( const plb_CurvedSurface& curve : level_data.curved_surfaces )
+	{
+		const plb_Material& material= level_data.materials[ curve.material_id ];
+		if( material.luminosity <= 0.0f )
+			continue;
+
+		const plb_ImageInfo& texture= level_data.textures[ material.light_texture_number ];
+
+		const unsigned int vertices_before= vertices.size();
+		GenCurveMesh( curve, level_data.curved_surfaces_vertices, vertices, indeces, normals );
+
+		for( unsigned int v= vertices_before; v < vertices.size(); v++ )
+		{
+			plb_Vertex& vertex= vertices[v];
+
+			vertex.lightmap_coord[0]= material.luminosity;
+
+			vertex.tex_maps[0]= texture.texture_array_id;
+			vertex.tex_maps[1]= texture.texture_layer_id;
+			vertex.tex_maps[2]= 255; // We do not need lightmap layer for polygons of this type
+		}
+	} // for curves
+
 	polygon_groups_[ int(PolygonType::Luminous) ].size= indeces.size() - index_cout_before;
 }
