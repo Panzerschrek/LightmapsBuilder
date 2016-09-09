f1c7f9a163a92df25bb5d14d96c61fe48414d397
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-09-09 22:12:26 +0700

    Šejder dlä pokaza tekstury adaptirovan pod novyju jarkostj.

--- shaders/texture_show_f.glsl
+++ shaders/texture_show_f.glsl
@@ -11,19 +11,5 @@ const float pi= 3.1415926535;
 
 void main()
 {
-	/*
-	vec3 coord;
-
-	float a= f_tex_coord.x * pi * 2.0;
-	float b= f_tex_coord.y * pi - pi * 0.5;
-	coord.y= sin(-b);
-	coord.x= coord.z= cos(b);
-	coord.x*= sin(a);
-	coord.z*= cos(a);
-
-	vec3 tex_data= texture( cubemap, coord ).xyz;
-	float mult= 0.1 * texture( cubemap_multiplier, -coord ).x;
-	color= vec4( tex_data * mult, 1.0 );
-	*/
-	color= 0.1 * texture( tex, f_tex_coord );
+	color= texture( tex, f_tex_coord );
 }
