01c3b8aa41942f5738a406adf25204db4f2f62b1
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-04-27 22:32:05 +0700

    Popravleny artefakty solnecnogo osvescenija.

--- shaders/sun_light_pass_f.glsl
+++ shaders/sun_light_pass_f.glsl
@@ -13,12 +13,12 @@ void main()
 	vec3 normalized_normal= normalize(f_normal);
 	float normal_vec_to_light_cos= dot(normalized_normal, light_dir);
 
-	float offset= 0.05 / min( 10.0, normal_vec_to_light_cos );
+	float offset= 0.05 / max( 0.01, normal_vec_to_light_cos );
 	vec3 pos_m= f_pos + light_dir * offset;
 
 	vec3 shadow_pos= ( view_matrix * vec4( pos_m, 1.0 ) ).xyz;
 	shadow_pos= shadow_pos * 0.5 + vec3( 0.5, 0.5, 0.5 );
 	float shadow_factor= texture( shadowmap, shadow_pos );
 
-	color= vec4( (normal_vec_to_light_cos * shadow_factor) * light_color, 1.0 );
+	color= vec4( (max( normal_vec_to_light_cos, 0.0 ) * shadow_factor) * light_color, 1.0 );
 }
